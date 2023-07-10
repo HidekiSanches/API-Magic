f9a1ef3c5145fbf67c0169951d9c75f8385da1b7
HidekiSanches <thiago.sanches@sptech.school>
2023-07-10 17:05:11 -0300

    Feat: Ambiente de desenvolvimento e produção

---
+++ user/src/main/java/com/api/user/Configuration/AppConfiguration.java
@@ -0,0 +1,34 @@
+package com.api.user.Configuration;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfiguration {
+    @Bean
+    @Profile("development")
+    public CommandLineRunner executarDevelopment(){
+        return args -> {
+            System.out.println("RODANDO O AMBIENTE DE DESENVOLVIMENTO");
+        };
+    }
+
+    @Bean
+    @Profile("production")
+    public CommandLineRunner executarProduction(){
+        return args -> {
+            System.out.println("RODANDO O AMBIENTE DE PRODUÇÃO");
+        };
+    }
+
+    @Bean
+    @Profile("development")
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
